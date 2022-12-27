2af79dd3ea1def6e9340a8a8dbc22cf4e7266dc4
RomanStass <roman.stasianok@cbitech.ae>
2022-12-27 13:21:34 +0300

    Run prettier

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,7 +7,8 @@ import {
   TOPIC_AUTH_REFRESH,
   TOPIC_AUTH_REGISTER,
   TOPIC_AUTH_REGISTER_BY_CODE,
-  TOPIC_AUTH_VERIFICATION, TOPIC_AUTH_VERIFICATION_CONNECT,
+  TOPIC_AUTH_VERIFICATION,
+  TOPIC_AUTH_VERIFICATION_CONNECT,
   TOPIC_AUTH_VERIFICATION_RESEND,
 } from '../users/constants';
 import { RegisterUserInput } from './dto/register-user.input';
@@ -62,10 +63,12 @@ export class AuthService implements OnModuleInit {
 
   verificationConnectUser(dto: VerificationUserInput): Promise<User> {
     return new Promise<User>((resolve, reject) => {
-      this.clientAuth.send(TOPIC_AUTH_VERIFICATION_CONNECT, { ...dto }).subscribe({
-        next: (response) => resolve(response),
-        error: (error) => reject(error),
-      });
+      this.clientAuth
+        .send(TOPIC_AUTH_VERIFICATION_CONNECT, { ...dto })
+        .subscribe({
+          next: (response) => resolve(response),
+          error: (error) => reject(error),
+        });
     });
   }
 
